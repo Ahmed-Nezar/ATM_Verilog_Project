f5cba23bd16bd506da0e5a75d6e54085217e10c9
Farida-Fakhry02 <21P0167@eng.asu.edu.eg>
2023-11-21 20:47:41 +0200

    Added the withdrawal task

--- Verilog_Code/functions.v
+++ Verilog_Code/functions.v
@@ -31,4 +31,22 @@ reg [31:0] current_balance;
     end
   endtask
 
+task withdrawAndUpdate;
+    input [15:0] amount;
+    input [31:0] currentBalance;
+    output [31:0] newBalance;
+    output success;
+
+    begin
+      if (amount <= currentBalance) begin
+        newBalance = currentBalance - amount;
+        success = 1;
+        $display("Withdrawal successful! New balance: %d", newBalance);
+      end else begin
+        newBalance = currentBalance;
+        success = 0;
+          $display("Withdrawal Unsuccessful Insufficient Funds ! New balance: %d", newBalance);
+      end
+    end
+  endtask
 endmodule
