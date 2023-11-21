a697479f56137b6c47687a410496363e18b0ae1c
Ahmed-Nezar <21P0025@eng.asu.edu.eg>
2023-11-21 16:16:06 +0200

    add: finding account numbers & adjusting mask

--- Verilog_Code/authenticator.v
+++ Verilog_Code/authenticator.v
@@ -1,8 +1,10 @@
 `include "definitions.v"
-module authenticator (acc_num, pin);
+module authenticator (acc_num, pin, acc_index_out, acc_found_stat);
 
 input [3:0] acc_num;
 input [15:0] pin;
+output reg [3:0] acc_index_out;
+output reg acc_found_stat;
 reg [10:0] acc_num_db [3:0];
 reg [10:0] pin_db [15:0];
 
@@ -18,6 +20,29 @@ initial begin
     acc_num_db[7] = 7; pin_db[7] = 8901;
     acc_num_db[8] = 8; pin_db[8] = 9012;
     acc_num_db[9] = 9; pin_db[9] = 7123;
+end
+
+// find the account number in the database & adjusting mask
+integer i;
+reg [3:0] acc_index;
+
+always @(*) begin
+    begin : find_account
+        
+        for (i = 0; i < 10 ; i = i +1 ) 
+            begin
+                if (acc_num_db[i] == acc_num) begin
+                    acc_index = i;
+                    acc_index_out = acc_index;
+                    acc_found_stat = `ACCOUNT_FOUND;
+                    disable find_account; // break the loop when finding the account
+                end
+                else begin
+                    acc_found_stat = `ACCOUNT_NOT_FOUND;
+                end
+            end
+    
+    end 
 end
 
 
