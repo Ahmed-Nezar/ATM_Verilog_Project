436ac44572eb057308d275907cb614a317528e0a
Monica Hany <126457536+Monica-hany@users.noreply.github.com>
2023-11-21 19:53:18 +0200

    adjusted compilation error in deposit

--- Verilog_Code/functions.v
+++ Verilog_Code/functions.v
@@ -1,33 +1,33 @@
 module ATM_Functions;
-  
+
   // Define registers for storing balance and deposit amount
   reg [31:0] current_balance;
-  reg [31:0] deposit_amount;
 
   // Deposit function
-    function deposit;
-        input [31:0] deposit_amt;  //amount to deposit
-        begin
-            int new_balance;
-            new_balance = current_balance + deposit_amt;
-            $display("Deposit successful! New balance: %d", new_balance);
-            return new_balance; // Return the updated balance
-        end
-    endfunction
-
-    // balance function
-    function void showBalanceInfo(input [31:0] balance, input bit includeReceipt);
-        // Assuming a 32-bit balance variable
-
-        // display the balance information
-        $display("Account Balance: $%0d.%02d", balance / 100, balance % 100);
-
-        // Check if a receipt is requested
-        if (includeReceipt) begin
-            $display("Receipt: Thank you for using our ATM. Have a nice day!");
-        end
-    endfunction
+  function reg[31:0] deposit;
+    input reg [31:0] deposit_amt;  // Amount to deposit
+    reg [31:0] new_balance;
+    begin
+      deposit = current_balance + deposit_amt; // Return the updated balance
+      $display("Deposit successful! New balance: %d", deposit);
+    end
+  endfunction
 
 
+  // Task to show balance info
+   task showBalanceInfo;
+    input reg [31:0] balance;
+    input reg includeReceipt; // Changed 'bit' to 'reg' for consistency
+
+    begin
+      // Display the balance information
+      $display("Account Balance: $%0d.%02d", balance / 100, balance % 100);
+      
+      // Check if a receipt is requested
+      if (includeReceipt) begin
+        $display("Receipt: Thank you for using our ATM. Have a nice day!");
+      end
+    end
+  endtask
 
 endmodule
