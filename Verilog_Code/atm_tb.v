781c6222ff0d3da1e91a6bb383f4ca6fb731c00f
Ahmed-Nezar <ahmednezarrr@gmail.com>
2023-12-05 15:43:39 +0200

    add: testcase for deposit for account 1

--- Verilog_Code/atm_tb.v
+++ Verilog_Code/atm_tb.v
@@ -49,7 +49,10 @@ module atm_tb;
         @(negedge clk);
         @(negedge clk);
 
-        rst = 1; operation = 3;
+        rst = 1; operation = 3; acc_num = 1; pin = 16'd1234; amount = 0; language = 0; Newpin = 0;
+        @(negedge clk);
+        @(negedge clk);
+        @(negedge clk);
         @(negedge clk);
 
         rst = 1; operation = 3; acc_num = 2; pin = 16'd2345; amount = 0; language = 0; Newpin = 0;
@@ -77,7 +80,7 @@ module atm_tb;
 
 
     initial begin
-        $monitor("clk=%b, rst=%b, operation=%d, acc_num=%d, pin=%d, amount=%d, language=%b, balance=%d, success=%b, state=%b", clk, rst, operation, acc_num, pin, amount, language, balance, success, state);
+        $monitor("operation=%d, acc_num=%d, pin=%d, amount=%d, language=%b, balance=%d, success=%b, state=%b", operation, acc_num, pin, amount, language, balance, success, state);
     end
 
 endmodule
