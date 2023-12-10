30a2ab4baf9e8ab43a6220259c5f655d5c4df55d
Ahmed-Nezar <ahmednezarrr@gmail.com>
2023-12-10 11:54:40 +0200

    update: making verilog code up-to-date with sv

--- Verilog_Code/authenticator.v
+++ Verilog_Code/authenticator.v
@@ -1,13 +1,14 @@
 `include "definitions.v"
-module Authenticator (acc_num, pin, acc_index_out, acc_found_stat, acc_auth_stat);
+module Authenticator (acc_num, pin, acc_index_out, acc_found_stat, acc_auth_stat ,newPin);
 input [3:0] acc_num;
 input [15:0] pin;
+input [15:0] newPin;
 output reg [3:0] acc_index_out;
 output reg acc_found_stat;
 output reg acc_auth_stat;
 reg [10:0] acc_num_db [9:0];
 reg [15:0] pin_db [9:0]; 
-
+integer fd;
 
 // intializing the account number and pin in decimal format as in reference model
 initial begin
@@ -43,7 +44,7 @@ always @(*) begin
     begin : check_pin
         if (acc_found_stat == `ACCOUNT_FOUND) begin
             
-            if (pin_db[acc_index] == pin) begin  // If the PIN matches the one in the database, set the authentication status to authenticated
+            if ((pin_db[acc_index] == pin )|| (pin_db[acc_index] == newPin)) begin  // If the PIN matches the one in the database, set the authentication status to authenticated
                 acc_auth_stat = `ACCOUNT_AUTHENTICATED;
                 $display("Account Pin authenticated",pin_db[acc_index]);
             end
@@ -68,6 +69,11 @@ end
         else begin
             $display("PIN changed successfully");
             pin_db[acc_index] = newPin;
+            fd = $fopen("./Database/pins.txt", "w"); 
+            for (i = 0; i < 10 ; i = i +1 ) begin
+                $fwrite(fd, "%b\n", pin_db[i]);
+            end
+            $fclose(fd);
             success = 1;
         end
     end
